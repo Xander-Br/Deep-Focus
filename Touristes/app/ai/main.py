ffe0934a8102372dc8acd9ece2397653a3873da7
Tatiana DEHON <182046@umons.ac.be>
2022-04-02 16:45:29 +0200

    Orientation_faces_operationnel
    Imprécision up/down

--- Touristes/app/ai/main.py
+++ Touristes/app/ai/main.py
@@ -5,36 +5,229 @@ from tensorflow import keras
 
 # Code from https://towardsdatascience.com/real-time-head-pose-estimation-in-python-e52db1bc606a
 
-""" =================== Modèle reconnaissance faciale =================== """
+
+
+""" =================== Modèle reconnaissance faciale - repris dans le modèle orientation de la tête =================== """
 
 # Instanciation du modèle DNN à utiliser
 modelFile = "../resources/models/res10_300x300_ssd_iter_140000.caffemodel"
 configFile = "../resources/models/deploy.prototxt.txt"
 net = cv2.dnn.readNetFromCaffe(configFile, modelFile)
 
-img = cv2.imread('../resources/test.jpg') # Load image de test => remplacer par frames par la suite
-h, w = img.shape[:2] # Récupère dimensions de l'image
-# On donne l'image au DNN (resize à 300x300 pour que ça fonctionne mieux)
-blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 1.0, (300, 300), (104.0, 117.0, 123.0))
-net.setInput(blob)
-# Recupère les visages identifiés
-faces = net.forward()
 
 
-# Pour dessiner les visages sur l'image
-for i in range(faces.shape[2]):
-        confidence = faces[0, 0, i, 2]
-        if confidence > 0.5:
-            box = faces[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (x, y, x1, y1) = box.astype("int")
-            cv2.rectangle(img, (x, y), (x1, y1), (0, 0, 255), 2)
-
-# test affichage image
-cv2.imshow("Test_image", img)
-cv2.waitKey(0)
 
 
-""" Modèle reconnaissance orientation de la tête """
+
+""" =================== Modèle reconnaissance orientation de la tête =================== """
+
+import math
+from face_detector import get_face_detector, find_faces
+from face_landmarks import get_landmark_model, detect_marks
+
+def get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val):
+    #Return the 3D points present as 2D for making annotation box
+    point_3d = []
+    dist_coeffs = np.zeros((4, 1))
+    rear_size = val[0]
+    rear_depth = val[1]
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, rear_size, rear_depth))
+    point_3d.append((rear_size, -rear_size, rear_depth))
+    point_3d.append((-rear_size, -rear_size, rear_depth))
+
+    front_size = val[2]
+    front_depth = val[3]
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d.append((-front_size, front_size, front_depth))
+    point_3d.append((front_size, front_size, front_depth))
+    point_3d.append((front_size, -front_size, front_depth))
+    point_3d.append((-front_size, -front_size, front_depth))
+    point_3d = np.array(point_3d, dtype=np.float).reshape(-1, 3)
+
+    # Map to 2d img points
+    (point_2d, _) = cv2.projectPoints(point_3d,
+                                      rotation_vector,
+                                      translation_vector,
+                                      camera_matrix,
+                                      dist_coeffs)
+    point_2d = np.int32(point_2d.reshape(-1, 2))
+    return point_2d
+
+def draw_annotation_box(img, rotation_vector, translation_vector, camera_matrix,
+                        rear_size=300, rear_depth=0, front_size=500, front_depth=400,
+                        color=(255, 255, 0), line_width=2):
+    """
+    Draw a 3D anotation box on the face for head pose estimation
+    Parameters
+    ----------
+    img : np.unit8
+        Original Image.
+    rotation_vector : Array of float64
+        Rotation Vector obtained from cv2.solvePnP
+    translation_vector : Array of float64
+        Translation Vector obtained from cv2.solvePnP
+    camera_matrix : Array of float64
+        The camera matrix
+    rear_size : int, optional
+        Size of rear box. The default is 300.
+    rear_depth : int, optional
+        The default is 0.
+    front_size : int, optional
+        Size of front box. The default is 500.
+    front_depth : int, optional
+        Front depth. The default is 400.
+    color : tuple, optional
+        The color with which to draw annotation box. The default is (255, 255, 0).
+    line_width : int, optional
+        line width of lines drawn. The default is 2.
+    Returns
+    -------
+    None.
+    """
+
+    rear_size = 1
+    rear_depth = 0
+    front_size = img.shape[1]
+    front_depth = front_size * 2
+    val = [rear_size, rear_depth, front_size, front_depth]
+    point_2d = get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
+    # # Draw all the lines
+    cv2.polylines(img, [point_2d], True, color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[1]), tuple(
+        point_2d[6]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[2]), tuple(
+        point_2d[7]), color, line_width, cv2.LINE_AA)
+    cv2.line(img, tuple(point_2d[3]), tuple(
+        point_2d[8]), color, line_width, cv2.LINE_AA)
+
+def head_pose_points(img, rotation_vector, translation_vector, camera_matrix):
+    """
+    Get the points to estimate head pose sideways
+    Parameters
+    ----------
+    img : np.unit8
+        Original Image.
+    rotation_vector : Array of float64
+        Rotation Vector obtained from cv2.solvePnP
+    translation_vector : Array of float64
+        Translation Vector obtained from cv2.solvePnP
+    camera_matrix : Array of float64
+        The camera matrix
+    Returns
+    -------
+    (x, y) : tuple
+        Coordinates of line to estimate head pose
+    """
+    rear_size = 1
+    rear_depth = 0
+    front_size = img.shape[1]
+    front_depth = front_size * 2
+    val = [rear_size, rear_depth, front_size, front_depth]
+    point_2d = get_2d_points(img, rotation_vector, translation_vector, camera_matrix, val)
+    y = (point_2d[5] + point_2d[8]) // 2
+    x = point_2d[2]
+
+    return (x, y)
+
+face_model = get_face_detector()
+landmark_model = get_landmark_model()
+cap = cv2.VideoCapture(0)
+ret, img = cap.read()
+size = img.shape
+font = cv2.FONT_HERSHEY_SIMPLEX
+# 3D model points.
+model_points = np.array([
+    (0.0, 0.0, 0.0),  # Nose tip
+    (0.0, -330.0, -65.0),  # Chin
+    (-225.0, 170.0, -135.0),  # Left eye left corner
+    (225.0, 170.0, -135.0),  # Right eye right corne
+    (-150.0, -150.0, -125.0),  # Left Mouth corner
+    (150.0, -150.0, -125.0)  # Right mouth corner
+])
+
+# Camera internals
+focal_length = size[1]
+center = (size[1] / 2, size[0] / 2)
+camera_matrix = np.array(
+    [[focal_length, 0, center[0]],
+     [0, focal_length, center[1]],
+     [0, 0, 1]], dtype="double"
+)
+while True:
+    ret, img = cap.read()
+    if ret == True:
+        faces = find_faces(img, face_model)
+        for face in faces:
+            marks = detect_marks(img, landmark_model, face)
+            # mark_detector.draw_marks(img, marks, color=(0, 255, 0))
+            image_points = np.array([
+                marks[30],  # Nose tip
+                marks[8],  # Chin
+                marks[36],  # Left eye left corner
+                marks[45],  # Right eye right corne
+                marks[48],  # Left Mouth corner
+                marks[54]  # Right mouth corner
+            ], dtype="double")
+            dist_coeffs = np.zeros((4, 1))  # Assuming no lens distortion
+            (success, rotation_vector, translation_vector) = cv2.solvePnP(model_points, image_points, camera_matrix, dist_coeffs, flags=cv2.SOLVEPNP_UPNP)
+
+            # Project a 3D point (0, 0, 1000.0) onto the image plane.
+            # We use this to draw a line sticking out of the nose
+
+            (nose_end_point2D, jacobian) = cv2.projectPoints(np.array([(0.0, 0.0, 1000.0)]), rotation_vector,
+                                                             translation_vector, camera_matrix, dist_coeffs)
+
+            for p in image_points:
+                cv2.circle(img, (int(p[0]), int(p[1])), 3, (0, 0, 255), -1)
+
+            p1 = (int(image_points[0][0]), int(image_points[0][1]))
+            p2 = (int(nose_end_point2D[0][0][0]), int(nose_end_point2D[0][0][1]))
+            x1, x2 = head_pose_points(img, rotation_vector, translation_vector, camera_matrix)
+
+            cv2.line(img, p1, p2, (0, 255, 255), 2)
+            cv2.line(img, tuple(x1), tuple(x2), (255, 255, 0), 2)
+            # for (x, y) in marks:
+            #     cv2.circle(img, (x, y), 4, (255, 255, 0), -1)
+            # cv2.putText(img, str(p1), p1, font, 1, (0, 255, 255), 1)
+            try:
+                m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+                ang1 = int(math.degrees(math.atan(m)))
+            except:
+                ang1 = 90
+
+            try:
+                m = (x2[1] - x1[1]) / (x2[0] - x1[0])
+                ang2 = int(math.degrees(math.atan(-1 / m)))
+            except:
+                ang2 = 90
+
+                # print('div by zero error')
+            if ang1 >= 48:
+                print('Head down')
+                cv2.putText(img, 'Head down', (30, 30), font, 2, (255, 255, 128), 3)
+            elif ang1 <= -48:
+                print('Head up')
+                cv2.putText(img, 'Head up', (30, 30), font, 2, (255, 255, 128), 3)
+
+            if ang2 >= 48:
+                print('Head right')
+                cv2.putText(img, 'Head right', (90, 30), font, 2, (255, 255, 128), 3)
+            elif ang2 <= -48:
+                print('Head left')
+                cv2.putText(img, 'Head left', (90, 30), font, 2, (255, 255, 128), 3)
+
+            cv2.putText(img, str(ang1), tuple(p1), font, 2, (128, 255, 255), 3)
+            cv2.putText(img, str(ang2), tuple(x1), font, 2, (255, 255, 128), 3)
+        # Display image
+        # cv2.imshow('img', img)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
+        break
+cv2.destroyAllWindows()
+cap.release()
 
 
 
