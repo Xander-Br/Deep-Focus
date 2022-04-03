e335d6687aedc86f4c7565e1cedf3ac4f2353768
LouisGobert <louis.gobert2000@gmail.com>
2022-04-03 06:27:26 +0200

    Design -  app

--- Touristes/ui2/src/app/app.component.ts
+++ Touristes/ui2/src/app/app.component.ts
@@ -1,35 +1,136 @@
-import { Component } from '@angular/core';
-import {Student} from "./dto/Student";
-import {Element} from "@angular/compiler";
+import { Component, OnInit, ElementRef, ViewChild } from "@angular/core";
+import { Student } from "./dto/Student";
+import {ScoresResponse} from "./dto/ScoresResponse";
+import { Element } from "@angular/compiler";
 import elements = chrome.devtools.panels.elements;
+import {
+    Chart,
+    CategoryScale,
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+} from "chart.js";
+import { interval, Subscription } from "rxjs";
+import { MatSort, Sort } from "@angular/material/sort";
+import { HttpClient } from "@angular/common/http";
 
+Chart.register(
+    LineController,
+    LineElement,
+    PointElement,
+    LinearScale,
+    Title,
+    CategoryScale
+);
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
-  title = 'ui2';
+export class AppComponent implements OnInit {
+    streams: Element[] = [];
+    displayedColumns: string[] = ["name", "score"];
+    averageScore = 81;
+    studentList: Student[] = [];
+    lb_state: string = "Attentif";
+    last_label: number = 0;
 
-  studentList: Student[] = [];
-  streams: Element[] = [];
+    // Test pour voir la mise en page
+    constructor(private elementRef: ElementRef, private httpClient: HttpClient) {
+        for (let i = 0; i < 40; i++) {
+            this.studentList.push({
+                id: 2,
+                name: "alexandre",
+                score: 0.9 - i * 0.02,
+            });
+        }
+    }
 
+    ngOnInit() {
+        this.chart_init();
+        this.setDelay(this.refresh_delay, this.labels, this.datas);
+    }
 
-  getAllStreams(){
-    chrome.tabs.query({active: true, currentWindow: true}, tabs => {
-      chrome.tabs.executeScript(tabs[0].id!, {
-        code: "document.getElementsByTagName('video');"
-      })
-    });
-  }
+    chart: any;
+    chart_init() {
+        const dataset = {
+            labels: [],
+            datasets: [
+                {
+                    label: "Class scores",
+                    data: [],
+                    borderColor: "rgb(75, 192, 192)",
+                    tension: 0.3,
+                },
+            ],
+        };
 
-  recieveAllStreams(resultArray: any){
-    console.log(resultArray[0]);
-  }
+        this.chart = new Chart("canvas", {
+            type: "line",
+            data: dataset,
+            options: {
+                scales: {
+                    y: {
+                        min: 0,
+                        max: 100,
+                    },
+                },
+            },
+        });
+    }
 
+    url_get_score: string = 'http://localhost:5000/get_score';
+    score_list: any;
 
+    refresh_delay: number = 10000;
+    labels: any = ["4", "5", "6", "7"];
+    datas: any = [20, 50, 90, 85];
+    setDelay(refresh_delay: any, labels: any, datas: any) {
+        // TO MODIFY - 4 ligne a supprimé
+        let label = labels.shift();
+        let data = datas.shift();
+        labels.push(label);
+        datas.push(data);
+
+        // Obtention de la liste des scores
+        this.httpClient.get<ScoresResponse>(this.url_get_score)
+            .subscribe(ScoreResponse => {
+               this. score_list = ScoreResponse.scores_list;
+            });
+
+        // TO MODIFY - decomment et delete +2
+        //let average_score = this.compute_average_score(this.score_list);
+        let average_score = 0.9;
+
+        this.lb_state = (average_score > 0.8) ?  "Attentif - " : "Inattentif - ";
+        this.lb_state += (average_score*100).toString() + "%";
+
+        this.addData(this.chart, data);
+
+        setTimeout(() => {
+            this.setDelay(refresh_delay, labels, datas);
+        }, refresh_delay);
+    }
+
+    compute_average_score(score_list: any) {
+        let sum = 0;
+        for (let key in score_list) {
+            sum += score_list[key];
+        }
+        return Object.keys(score_list).length;
+    }
+
+    addData(chart: Chart, average_score: any) {
+        this.last_label += 1;
+
+        // @ts-ignore
+        chart.data.labels.push(this.last_label.toString() + "m");
+        chart.data.datasets.forEach((dataset) => {
+            dataset.data.push(average_score);
+        });
+        chart.update();
+    }
 }
-
-
-
--- Touristes/ui2/src/app/app.module.ts
+++ Touristes/ui2/src/app/app.module.ts
@@ -4,6 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
+import {MatTableModule} from "@angular/material/table";
+import {MatSortModule} from "@angular/material/sort";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+import {MatExpansionModule} from "@angular/material/expansion";
+import { HttpClientModule } from '@angular/common/http';
+
+import { Chart } from 'chart.js';
 
 @NgModule({
   declarations: [
@@ -12,7 +20,13 @@ import {MatButton, MatButtonModule} from "@angular/material/button";
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
+    MatSortModule,
+    MatProgressBarModule,
+    MatExpansionModule,
+      HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
